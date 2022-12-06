c5aa876add96a1b9533b0df1d6f4e9a84caf74dd
unknown <vaduvacalin@yahoo.com>
2022-12-06 15:25:20 +0200

    Initial Commit

---
+++ PoriectSD/PoriectSD/Services/EnergyServices.cs
@@ -0,0 +1,19 @@
+﻿using PoriectSD.Database;
+using PoriectSD.Models;
+
+namespace PoriectSD.Services
+{
+    public class EnergyServices : IEnergyServices
+    {
+        SDDbContext _dbContext;
+        public EnergyServices(SDDbContext SDDbContext)
+        {
+            _dbContext = SDDbContext;
+        }
+
+        public async Task<List<Energy>> GetEnergyForDeviceId(int id, DateTime date)
+        {
+            return _dbContext.Energies.Where(e => (e.DeviceId == id) && (e.Timestamp.Date == date.Date)).ToList();
+        }
+    }
+}
